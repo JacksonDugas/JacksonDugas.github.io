d4b0bed4ba03d948316ae9f86ff4e69640a4a66f
JacksonDugas <jdd8549988@lpssonline.com>
2023-09-11 15:49:09 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
   }; dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  for (var i - 0; i < dataShapes.length; i++) {
+  for (var i = 0; i < dataShapes.length; i++) {
     var currentShape = dataShapes[i];
 
     if (currentShape.color === "red") {
@@ -66,7 +66,7 @@ $(document).ready(function () {
   // TODO 4-a: add a function that handles the good display type
   function handleGood(color, shape, repeat){
     setBackgroundWithSimple(color, shape, repeat);
-    animationDetails.displayTyp = 2;
+    animationDetails.displayType = 2;
   }
 
   // TODO 5-a: add a function that handles the bad display type
